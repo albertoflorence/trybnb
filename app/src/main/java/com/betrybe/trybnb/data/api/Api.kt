251851981eb77feb6cc76b9296029181ff9f30c6
albertoflorence <alberto.sabatier@gmail.com>
2024-03-25 13:08:16 -0300

    requisito 6

---
+++ app/src/main/java/com/betrybe/trybnb/data/api/Api.kt
@@ -0,0 +1,21 @@
+package com.betrybe.trybnb.data.api
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Api {
+    private var retrofit: Retrofit? = null
+    fun getInstance(): ApiService {
+        if (retrofit == null) {
+
+            val client = OkHttpClient.Builder().build()
+            retrofit =
+                Retrofit.Builder().baseUrl("https://restful-booker.herokuapp.com/auth/")
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create()).build()
+        }
+
+        return retrofit?.create(ApiService::class.java)!!
+    }
+}
--- app/src/main/java/com/betrybe/trybnb/ui/views/activities/ProfileFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/activities/ProfileFragment.kt
@@ -1,20 +1,33 @@
 package com.betrybe.trybnb.ui.views.activities
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.betrybe.trybnb.R
+import com.betrybe.trybnb.common.ApiIdlingResource
+import com.betrybe.trybnb.data.api.Api
+import com.betrybe.trybnb.data.models.AuthRequest
 import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ProfileFragment : Fragment() {
 
     private lateinit var mLoginInputProfile: TextInputLayout
     private lateinit var mPasswordInputProfile: TextInputLayout
     private lateinit var mLoginButtonProfile: Button
+    private lateinit var mLoginMessage: TextView
+
+    private val api = Api.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,6 +37,7 @@ class ProfileFragment : Fragment() {
         mLoginInputProfile = view.findViewById(R.id.login_input_profile)
         mPasswordInputProfile = view.findViewById(R.id.password_input_profile)
         mLoginButtonProfile = view.findViewById(R.id.login_button_profile)
+        mLoginMessage = view.findViewById(R.id.login_message)
 
         handleLogin()
 
@@ -32,11 +46,12 @@ class ProfileFragment : Fragment() {
 
     private fun handleLogin() {
         mLoginButtonProfile.setOnClickListener {
-            validateLogin()
+            if(!validateLogin()) return@setOnClickListener
+            authentication()
         }
     }
 
-    private fun validateLogin() {
+    private fun validateLogin(): Boolean {
         mLoginInputProfile.error =
             if (mLoginInputProfile.editText?.text.isNullOrBlank())
                 "O campo Login é obrigatório"
@@ -46,5 +61,29 @@ class ProfileFragment : Fragment() {
             if (mLoginInputProfile.editText?.text.isNullOrBlank())
                 "O campo Password é obrigatório"
             else null
+
+        return mLoginInputProfile.error == null || mPasswordInputProfile.error == null
+    }
+
+    private fun authentication() {
+        val login = mLoginInputProfile.editText?.text.toString()
+        val password = mPasswordInputProfile.editText?.text.toString()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                ApiIdlingResource.increment()
+                withContext(Dispatchers.Main) {
+                    val response = api.auth(AuthRequest(login, password))
+                    if (response.isSuccessful) {
+                        val data = response.body() ?: return@withContext
+                        mLoginMessage.visibility = View.VISIBLE
+                    }
+                    ApiIdlingResource.decrement()
+                }
+            } catch (ex: Exception) {
+                ApiIdlingResource.decrement()
+                ex.printStackTrace()
+            }
+        }
     }
 }
